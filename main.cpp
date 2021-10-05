447e74d35191a0e0f20858620e7c3895d19cd8c6
Gonzalo Lopez andres <glopez-a@c2r10s2.42madrid.com>
2021-10-05 17:52:58 +0200

    servidor de prueba

--- main.cpp
+++ main.cpp
@@ -1,6 +1,13 @@
-#include <iostream>
+#include <stdio.h>
 #include <sys/socket.h>
+#include <unistd.h>
+#include <stdlib.h>
 #include <netinet/in.h>
+#include <string.h>
+#include <iostream>
+#include <thread>
+
+// 
 
 #define PORT 6667
 
@@ -15,15 +22,18 @@ int main(int argc, char **argv)
 {
     struct sockaddr_in address;
 
+    long valread;
     int server_fd, new_socket;
     int addrlen = sizeof(address);
+    char *hello = "Hello from server";
+
     if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) < 0)
         return (ft_error("Socket failed"));
 
 
     memset((char *)&address, 0, sizeof(address));
     address.sin_family = AF_INET; 
-    address.sin_addr.s_addr = htonl(INADDR_ANY); 
+    address.sin_addr.s_addr = htonl(INADDR_ANY);
     address.sin_port = htons(PORT);
     
     if (bind(server_fd, (struct sockaddr *)&address, sizeof(address)) < 0)
@@ -42,8 +52,13 @@ int main(int argc, char **argv)
         if ((new_socket = accept(server_fd, (struct sockaddr *)&address, (socklen_t*)&addrlen)) < 0)
         {
             std::cout << "In accept\n";
-            exit(EXIT_FAILURE);        
+            continue;        
         }
-        
+        char buffer[30000] = {0};
+        valread = read( new_socket , buffer, 30000);
+        printf("%s\n",buffer );
+        write(new_socket , hello , strlen(hello));
+        printf("------------------Hello message sent-------------------\n");
+        close(new_socket);
     }
 }
